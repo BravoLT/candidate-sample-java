381c8076fd9738e2abc0d6dda4ece3d7179668a6
EricRybarczyk <EricRybarczyk@gmail.com>
2022-07-27 20:45:25 -0400

    Unit Tests for Payment Controller Retrieve API

--- src/main/java/com/bravo/user/controller/PaymentController.java
+++ src/main/java/com/bravo/user/controller/PaymentController.java
@@ -3,6 +3,7 @@ package com.bravo.user.controller;
 import com.bravo.user.annotation.SwaggerController;
 import com.bravo.user.model.dto.PaymentDto;
 import com.bravo.user.service.PaymentService;
+import com.bravo.user.validator.UserValidator;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,17 +16,17 @@ import java.util.List;
 public class PaymentController {
 
     private final PaymentService paymentService;
+    private final UserValidator userValidator;
 
-    public PaymentController(PaymentService paymentService) {
+    public PaymentController(PaymentService paymentService, UserValidator userValidator) {
         this.paymentService = paymentService;
+        this.userValidator = userValidator;
     }
 
     @GetMapping(value = "/retrieve/{userId}")
     @ResponseBody
     public List<PaymentDto> retrieve(final @PathVariable String userId) {
-
-        // TODO: validate userId
-
+        userValidator.validateId(userId);
         return paymentService.retrieveByUserId(userId);
     }
 }
