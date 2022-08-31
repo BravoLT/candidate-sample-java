4f1bfaa24d994f1448138722ab3c4a15f977c914
Mikedeejay2 <58639173+Mikedeejay2@users.noreply.github.com>
2022-08-31 18:11:46 -0400

    Implementation of "add payment method retrieve api"

---
+++ src/main/java/com/bravo/user/controller/PaymentController.java
@@ -0,0 +1,29 @@
+package com.bravo.user.controller;
+
+import com.bravo.user.annotation.SwaggerController;
+import com.bravo.user.model.dto.PaymentDto;
+import com.bravo.user.service.PaymentService;
+import com.bravo.user.validator.UserValidator;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequestMapping(value = "/payment")
+@SwaggerController
+public class PaymentController {
+
+  private final PaymentService paymentService;
+  private final UserValidator userValidator;
+
+  public PaymentController(PaymentService paymentService, UserValidator userValidator) {
+    this.paymentService = paymentService;
+    this.userValidator = userValidator;
+  }
+
+  @GetMapping(value = "/retrieve/{userId}")
+  @ResponseBody
+  public List<PaymentDto> retrieve(final @PathVariable String userId) {
+    userValidator.validateId(userId);
+    return paymentService.retrieveByUserId(userId);
+  }
+}
